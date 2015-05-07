e0695e0c4f28b8dcee541f168aeeee7ce44b01a3
Joshua Soileau <joshua.soileau@gmail.com>
2015-05-07 14:22:12 -0400

    CNAME Change - Style Changes

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var pageContent = 	document.getElementById('page-content'),
+	winHeight 	= 	window.innerHeight,
+	blur		=	document.getElementById('blurred'),
+	blurWrap 	=	document.getElementById('blur-wrap'),
+	translationStyle; 
+
+
+// Controls blurred elements in header
+// if not on mobile
+if(typeof window.orientation == 'undefined'){
+	// copy & paste content into blurred element
+	blurWrap.innerHTML = pageContent.outerHTML;
+	// manually set the hieght of the new content
+	blur.style.height = winHeight + 'px';
+
+	window.addEventListener('scroll', function() {
+		// get new scroll Y position
+		var scrollTop = (document.all) ? document.body.scrollTop : window.pageYOffset;
+		// inline style attribute for translation of blurred content
+		translationStyle = 'translate3d(0,' + (-scrollTop + 'px') + ',0)';
+		blurWrap.style.cssText = '-webkit-transform:' + translationStyle
+								+ ';-moz-transform:' + translationStyle
+								+ ';transform:' + translationStyle
+								+ ';height:' + winHeight + 'px';
+	});
+}
